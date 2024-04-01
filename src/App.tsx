b961a4c509f0e4e4dad78c268e4232b3f50700a1
Artur <alimayko91@gmail.com>
2024-04-01 12:21:54 +0300

    add UseEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React, {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import {Button} from "./Components/Button/Button";
 
 function App() {
-    const [currentCount, setCurrentCount] = useState(0);
+    const [currentCount, setCurrentCount] = useState<number>(0);
+
+
+    useEffect( () => {
+        let currentCountAsString  = localStorage.getItem('currentCount')
+        if(currentCountAsString) {
+            let newCurrentCount = JSON.parse(currentCountAsString)
+            setCurrentCount(newCurrentCount)
+        }
+    },[])
+
+    useEffect( () => {
+        localStorage.setItem('currentCount', JSON.stringify(currentCount));
+    },[currentCount])
 
     const onClickButtonHandler = () => {
         if (currentCount !== 5) {
@@ -11,41 +24,17 @@ function App() {
         }
     }
 
-    const setToLocalStorageHandler = () => {
-        localStorage.setItem('currentCount', JSON.stringify(currentCount));
-    }
-
-    const getFromLocalStorageHandler = () => {
-        let currentCountAsString  = localStorage.getItem('currentCount')
-        if(currentCountAsString) {
-            let newCurrentCount = JSON.parse(currentCountAsString)
-            setCurrentCount(newCurrentCount)
-        }
-    }
-
-    //удаление localStorage
-    // const clearLocalStorageHandler = () => {
-    //     localStorage.clear()
-    //     setCurrentCount(0)
-    // }
-    //
+    const onHandleResetOnCLick = () => setCurrentCount(0)
 
     return (
         <div className="App">
             <div className='count-wrapper'>
                 <h1 className={currentCount === 5 ? 'colorCount' : ''}>{currentCount}</h1>
             </div>
-
             <div className='button-wrapper'>
-                <Button disabled={currentCount >= 5} name={'inc'} callBack={onClickButtonHandler}/>
+                <Button disabled={currentCount >= 5} name='inc' callBack={onClickButtonHandler}/>
 
-                <Button disabled={currentCount <= 0} name={'reset'} callBack={() => {
-                    setCurrentCount(0)
-                }}/>
-
-                <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-                <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-                {/*<button onClick={clearLocalStorageHandler}>clearLocalStorage</button>*/}
+                <Button disabled={currentCount <= 0} name='reset' callBack={onHandleResetOnCLick}/>
             </div>
         </div>
     );
