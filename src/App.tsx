41e0d13cd104aad12989a3197a5665461dd3304c
Artur <alimayko91@gmail.com>
2024-04-01 11:37:13 +0300

    use localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from "./Components/Button/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentCount, setCurrentCount] = useState(0);
+
+    const onClickButtonHandler = () => {
+        if (currentCount !== 5) {
+            setCurrentCount(currentCount + 1)
+        }
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('currentCount', JSON.stringify(currentCount));
+    }
+
+    const getFromLocalStorageHandler = () => {
+        let currentCountAsString  = localStorage.getItem('currentCount')
+        if(currentCountAsString) {
+            let newCurrentCount = JSON.parse(currentCountAsString)
+            setCurrentCount(newCurrentCount)
+        }
+    }
+
+    //удаление localStorage
+    // const clearLocalStorageHandler = () => {
+    //     localStorage.clear()
+    //     setCurrentCount(0)
+    // }
+    //
+
+    return (
+        <div className="App">
+            <div className='count-wrapper'>
+                <h1 className={currentCount === 5 ? 'colorCount' : ''}>{currentCount}</h1>
+            </div>
+
+            <div className='button-wrapper'>
+                <Button disabled={currentCount >= 5} name={'inc'} callBack={onClickButtonHandler}/>
+
+                <Button disabled={currentCount <= 0} name={'reset'} callBack={() => {
+                    setCurrentCount(0)
+                }}/>
+
+                <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+                <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+                {/*<button onClick={clearLocalStorageHandler}>clearLocalStorage</button>*/}
+            </div>
+        </div>
+    );
 }
 
 export default App;
